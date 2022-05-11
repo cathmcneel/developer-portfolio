f282ddac44fc4f795314c9aa768b85dfc5a32eb7
Catherine McNeel <catherinemcneel@icloud.com>
2022-05-10 20:55:14 -0400

    small updates to structuee

--- developer-portfolio/src/App.js
+++ developer-portfolio/src/App.js
@@ -3,41 +3,7 @@ import PortfolioMain from "./components/PortfolioMain"
 
 function App() {
   <PortfolioMain />;
-  // const [categories] = useState([
-  //   // { name: 'Catherine McNeel', description: '' },
-  //   { name: 'Portfolio', description: 'View previously completed projects' },
-  //   { name: 'Resume', description: 'Download my Resume here' },
-  // ]);
 
-  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  // const [contactSelected, setContactSelected] = useState(false);
-
-//   return (
-//     <div>
-//       <Nav
-//         categories={categories}
-//         setCurrentCategory={setCurrentCategory}
-//         currentCategory={currentCategory}
-//         contactSelected={contactSelected}
-//         setContactSelected={setContactSelected}
-//       ></Nav>
-//       <main>
-//         {!contactSelected ? (
-//           <>
-//             <About></About>
-//             <Portfolio currentCategory={currentCategory}></Portfolio>
-//           </>
-         
-//         ) : (
-//           <ContactForm></ContactForm>
-        
-//         ) 
-        
-//         }
-//       </main>
-//     </div>
-//   );
  }
 
 export default App;
